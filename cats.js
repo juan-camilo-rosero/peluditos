794748b8e219d57bf251adc03a773bc68c5e0c4a
juan-camilo-rosero <128432617+juan-camilo-rosero@users.noreply.github.com>
2024-01-12 13:40:15 -0500

    Add files via upload

--- cats.js
+++ cats.js
@@ -1,8 +1,38 @@
 import { getCats } from "./stripe_api.js"
+import { appearDiv, dissappearDiv } from "./transitions.js"
+import { changeToCreateAccount, createUser, validateUser } from "./account.js"
+import { validateAccountInfo } from "./validations.js"
 
-const d = document
+const d = document,
+$closeLogin = d.querySelector(".close-account"),
+$openLogin = d.querySelector(".user-img"),
+$loginBtn = d.querySelector(".login-btn"),
+$user = d.querySelector("#email-account"),
+$password = d.querySelector("#password-account"),
+$changeToCreate = d.querySelector(".create-account")
 
 d.addEventListener("DOMContentLoaded", e => {
     getCats(".cats")
     d.querySelector(".close").addEventListener("click", e => d.getElementById("adopt-form").classList.add("hidden"))
+    $closeLogin.addEventListener("click", e => dissappearDiv("#account"))
+    $openLogin.addEventListener("click", e => appearDiv("#account"))
+    $changeToCreate.addEventListener("click", e => changeToCreateAccount())
+    $loginBtn.addEventListener("click", async e => {
+        if($loginBtn.getAttribute("data-state") === "login"){
+            if(!validateAccountInfo($user.value, $password.value)) return alert("usuario o contraseña inválidos")
+            const res = await validateUser($user.value, $password.value)
+            if(res){
+                dissappearDiv("#account")
+                alert("Inicio de sesión exitoso")
+            }
+        }
+        else if($loginBtn.getAttribute("data-state") === "create"){
+            if(!validateAccountInfo($user.value, $password.value)) return alert("usuario o contraseña inválidos")
+            const res = await createUser($user.value, $password.value)
+            if(res){
+                dissappearDiv("#account")
+                alert("Usuario creado exitosamente")
+            }
+        }
+    })
 })
--- leave.js
+++ leave.js
@@ -1,9 +1,17 @@
 import { createCat } from "./stripe_api.js"
-import { showImage, uploadToImgBB, validateInputs } from "./validations.js"
+import { appearDiv, dissappearDiv } from "./transitions.js"
+import { showImage, uploadToImgBB, validateInputs, validateAccountInfo } from "./validations.js"
+import { changeToCreateAccount, createUser, validateUser } from "./account.js"
 
 const d = document,
 $leaveBtn = d.querySelector(".leave"),
-$fileInput = d.getElementById("image")
+$fileInput = d.getElementById("image"),
+$closeLogin = d.querySelector(".close-account"),
+$openLogin = d.querySelector(".user-img"),
+$loginBtn = d.querySelector(".login-btn"),
+$user = d.querySelector("#email-account"),
+$password = d.querySelector("#password-account"),
+$changeToCreate = d.querySelector(".create-account")
 
 d.addEventListener("DOMContentLoaded", e => {
     $leaveBtn.addEventListener("click", e => {
@@ -24,4 +32,25 @@ d.addEventListener("DOMContentLoaded", e => {
         }
     })
     showImage(".show-image", "#image")
+    $closeLogin.addEventListener("click", e => dissappearDiv("#account"))
+    $openLogin.addEventListener("click", e => appearDiv("#account"))
+    $changeToCreate.addEventListener("click", e => changeToCreateAccount())
+    $loginBtn.addEventListener("click", async e => {
+        if($loginBtn.getAttribute("data-state") === "login"){
+            if(!validateAccountInfo($user.value, $password.value)) return alert("usuario o contraseña inválidos")
+            const res = await validateUser($user.value, $password.value)
+            if(res){
+                dissappearDiv("#account")
+                alert("Inicio de sesión exitoso")
+            }
+        }
+        else if($loginBtn.getAttribute("data-state") === "create"){
+            if(!validateAccountInfo($user.value, $password.value)) return alert("usuario o contraseña inválidos")
+            const res = await createUser($user.value, $password.value)
+            if(res){
+                dissappearDiv("#account")
+                alert("Usuario creado exitosamente")
+            }
+        }
+    })
 })
